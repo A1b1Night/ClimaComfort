9e93dfdf1fa2f31e9488fe8970f9d8bb810e0676
A1Jhonas <carlos_55232@hotmail.com>
2023-08-10 13:09:51 -0500

    Menu

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+// Estilos
+import './Header.css'
+
+//Importamos el logo
+import Logo from '../../Assets/Logo.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <div className='nav-principal'>
+        <div className='logo'>
+          <img src={Logo} alt="img" className="w-20" />
+          <h2>Clima-Comfort</h2>
+        </div>
+        <nav className='navegacion'>
+          <NavLink to="/" >¿Quienes Somos?</NavLink>
+          <NavLink to="/" >Contactenos</NavLink>
+          <NavLink to="/" >Nuestro Trabajo</NavLink>
+          <NavLink to="/" >Gary haz tu parte</NavLink>
+        </nav>
+      </div>
+    </div>
   )
 }
 
